c517648a21ba1d3a4a6b35c9882a7d5f207b320f
FlaviusDinescu <f_dinescu@yahoo.com>
2019-09-04 12:43:25 +0300

    test

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/test-post', function (req, res) {
-    var params={
-        creditCardNumber:"",
-        cardholder:"",
-        expirationDate:"",
-        securityCode:"",
-        amount:""
+    var params = {
+        creditCardNumber: "",
+        cardholder: "",
+        expirationDate: "",
+        securityCode: "",
+        amount: "" //
     }
     params.creditCardNumber = req.body.creditCardNumber;
     params.cardholder = req.body.cardholder;
     params.expirationDate = req.body.expirationDate;
     params.securityCode = req.body.securityCode;
     params.amount = req.body.amount;
-    
-    if(params.creditCardNumber && params.cardholder && params.expirationDate && params.amount){
-        res.status(200).send({success:true,values:params});
-    }else{
-        res.status(400).send({success:false,message:"Error in parameters"});
+
+    if (params.creditCardNumber && params.cardholder && params.expirationDate && params.amount) {
+        res.status(200).send({ success: true, values: params });
+    } else {
+        res.status(400).send({ success: false, message: "Error in parameters" });
     }
 });
 
